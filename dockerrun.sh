0832989d11b335919743e637034cc1b254d654a5
Ruchir <ruchirkadam1729@gmail.com>
2026-04-14 22:36:44 +0530

    removed dockerfiles

--- dockerrun.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p "$(pwd)/checkpoints"
-docker run -it --rm \
-  -p 7860:7860 \
-  -e GROQ_API_KEY=$(cat secrets/GROQ_API_KEY) \
-  -v "$(pwd)/checkpoints:/app/checkpoints" \
-  mgmt-assignment-huggingface-app
